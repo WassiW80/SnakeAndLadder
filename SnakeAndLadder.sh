1a0e3d33aef1bc93e93398096fcbd9340682e7f2
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-01 15:15:57 +0530

    ADDED:Functionalities for ladder, snake and no-play.

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,10 +1,31 @@
 #!/bin/bash -x
 echo "Welcome to Snake And Ladder"
+#Constant
 INITIAL_POSITION=0
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+#Variable
 player1=$INITIAL_POSITION
+
+function ladderSnakeOrNoPlayChecking() {
+	option=$((RANDOM%3))
+	case $option in
+		$NO_PLAY)
+			PLAYER1=$PLAYER1
+			;;
+		$LADDER)
+			PLAYER1+=$numberOnDie
+			;;
+		$SNAKE)
+			PLAYER1=$(($PLAYER1-$numberOnDie))
+			;;
+	esac
+}
 
 function dieRoll() {
    numberOnDie=$(($RANDOM%6+1))
+	ladderSnakeOrNoPlayChecking
 }
 dieRoll
 
